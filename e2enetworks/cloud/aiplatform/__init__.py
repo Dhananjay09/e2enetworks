3535fee01f5a0fdf48abcfdc549ec52db973fafd
dhananjay.singh <dhananjay.singh@e2enetworks.com>
2023-05-29 15:02:33 +0530

    Added API token also in cloud

--- e2enetworks/cloud/aiplatform/__init__.py
+++ e2enetworks/cloud/aiplatform/__init__.py
@@ -4,3 +4,4 @@ from e2enetworks.cloud.aiplatform.init import init
 from e2enetworks.cloud.aiplatform.Model import Models
 from e2enetworks.cloud.aiplatform.Project import Projects
 from e2enetworks.cloud.aiplatform.Teams import Teams
+from e2enetworks.cloud.aiplatform.APIToken import APITokens
