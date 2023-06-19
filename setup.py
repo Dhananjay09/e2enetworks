1dc25138d3999a9a2571561132d11920a415e0f1
dhananjay.singh <dhananjay.singh@e2enetworks.com>
2023-06-19 14:19:54 +0530

    Changed version of sdk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 from os import path
 
-version = "0.1.1"
+version = "0.1.2"
 install_requires = [
     "requests",
     "urllib3==1.26.6",
